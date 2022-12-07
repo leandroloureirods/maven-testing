848eaabde0d6b91e3be61a2b94549c20637ad1c8
Leandro Loureiro dos Santos <leandroloureirods@gmail.com>
2022-12-07 13:44:18 -0500

    Testando validação de CPF Caelum-Stella

--- src/main/java/br/com/alura/maven/testing/App.java
+++ src/main/java/br/com/alura/maven/testing/App.java
@@ -1,6 +1,31 @@
 package br.com.alura.maven.testing;
-public class App {	
+
+import br.com.caelum.stella.validation.CPFValidator;
+
+public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello");		
-	}	
+		String cpf = "";
+		boolean cpfValido = false;
+
+		cpf = "07800812774";
+		cpfValido = valida(cpf);
+		System.out.printf("O CPF informado %s é %s.\n", cpf, cpfValido ? "válido" : "inváldo");
+
+		cpf = "11111111111";
+		cpfValido = valida(cpf);
+		System.out.printf("O CPF informado %s é %s.\n", cpf, cpfValido ? "válido" : "inválido");
+
+	}
+
+	public static boolean valida(String cpf) {
+		CPFValidator cpfValidator = new CPFValidator();
+		try {
+			cpfValidator.assertValid(cpf);
+			return true;
+		} catch (Exception e) {
+			// e.printStackTrace();
+			return false;
+		}
+	}
+
 }
